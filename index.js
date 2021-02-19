510a31086e060ddfc14bca6f1ec1c5c48c9d926b
shinn92kr <64060313+shinn92kr@users.noreply.github.com>
2021-02-19 16:32:33 +0900

    #2.3_Creating Server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("HELLO")
+// IMPORT EXPRESS MODULE
+const express = require("express");
+// EXPRESS 실행
+const app = express();
+const PORT = 4000;
+
+function handleListening() {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
