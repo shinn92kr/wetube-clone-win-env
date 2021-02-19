bd3603f0d822d7d2d3facf5b7641aa1581462e8e
shinn92kr <64060313+shinn92kr@users.noreply.github.com>
2021-02-19 18:32:57 +0900

    #2.5_babel install

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 // IMPORT EXPRESS MODULE
-const express = require("express");
+import express from "express";
 // EXPRESS 실행
 const app = express();
 const PORT = 4000;
 
-function handleListening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
-
-function handleHome(req, res) {
-    console.log("Hi from HOME!");
-    console.log(req);
-    res.send("HELLO FROM HOME!!");
-}
-function handleProfile(req, res) {
-    res.send("You are in PROFILE!");
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+const handleHome = (req, res) => res.send("HELLO from HOME!");
+const handleProfile = (req, res) => res.send("You are in PROFILE!");
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
