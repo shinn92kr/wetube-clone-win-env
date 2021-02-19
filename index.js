d3e47feedea5e9baa2084b33ecd43b7017bcf274
shinn92kr <64060313+shinn92kr@users.noreply.github.com>
2021-02-19 16:55:23 +0900

    #2.4_makind server_avoiding CANNOT GET

--- index.js
+++ index.js
@@ -8,4 +8,16 @@ function handleListening() {
     console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+    console.log("Hi from HOME!");
+    console.log(req);
+    res.send("HELLO FROM HOME!!");
+}
+function handleProfile(req, res) {
+    res.send("You are in PROFILE!");
+}
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
