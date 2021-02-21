9cff667d974c323e59e390007cc6501e58cde387
shinn92kr <64060313+shinn92kr@users.noreply.github.com>
2021-02-21 18:16:43 +0900

    #2.6_MiddleWare-theory

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 // IMPORT EXPRESS MODULE
 import express from "express";
-// EXPRESS 실행
+// EXPRESS 객체 생성
 const app = express();
 const PORT = 4000;
 
+// Controller
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 const handleHome = (req, res) => res.send("HELLO from HOME!");
 const handleProfile = (req, res) => res.send("You are in PROFILE!");
 
+const betweenHome = (req, res, next) => {
+    console.log("I'm between");
+    next();
+};
+
+// MiddleWare
+app.use(betweenHome);
+
+// ROUTE
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 
+// # Express 실행
 app.listen(PORT, handleListening);
