edaf47242d364b3d66de08da6f2369c2aa3823ea
shinn92kr <64060313+shinn92kr@users.noreply.github.com>
2021-02-21 19:01:16 +0900

    #2.7_Mode MiddleWare Install

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // IMPORT EXPRESS MODULE
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 // EXPRESS 객체 생성
 const app = express();
 const PORT = 4000;
@@ -9,17 +14,16 @@ const handleListening = () => console.log(`Listening on: http://localhost:${PORT
 const handleHome = (req, res) => res.send("HELLO from HOME!");
 const handleProfile = (req, res) => res.send("You are in PROFILE!");
 
-const betweenHome = (req, res, next) => {
-    console.log("I'm between");
-    next();
-};
-
-// MiddleWare
-app.use(betweenHome);
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(helmet());
+app.use(morgan("dev"));
 
 // ROUTE
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 
-// # Express 실행
+// Express 실행
 app.listen(PORT, handleListening);
