b68b3d29fcc060f4c970e1e1e0cf27ded8e39d34
shinn92kr <64060313+shinn92kr@users.noreply.github.com>
2021-03-05 00:45:23 +0900

    3.1_Connection to MongoDB

--- db.js
+++ db.js
@@ -1,3 +1,16 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/we-tube", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("▶ Connected to DB");
+const handleError = () => console.log("❌ Error on DB Connenction");
+db.once("open", handleOpen);
+db.on("error", handleError);
 // export const videos = [
 //     {
 //         id: 123456,
