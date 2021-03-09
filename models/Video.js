3260222d5395e17e9831f4334a9ef0b0c1a491c1
shinn92kr <64060313+shinn92kr@users.noreply.github.com>
2021-03-09 20:38:17 +0900

    3.4_Comment Model

--- models/Video.js
+++ models/Video.js
@@ -19,6 +19,12 @@ const VideoSchema = new mongoose.Schema({
         type: Date,
         default: Date.now,
     },
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment",
+        },
+    ],
 });
 
 // Model
