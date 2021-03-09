9337f371df9e44ee9255ed9044f45e8c0cf96189
shinn92kr <64060313+shinn92kr@users.noreply.github.com>
2021-03-09 20:23:27 +0900

    3.3_Making Video Model

---
+++ models/Video.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+// Schema
+const VideoSchema = new mongoose.Schema({
+    fileUrl: {
+        type: String,
+        required: "File URL is required",
+    },
+    title: {
+        type: String,
+        required: "Title is required",
+    },
+    description: String,
+    views: {
+        type: Number,
+        default: 0,
+    },
+    createrdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+// Model
+const model = mongoose.model("Video", VideoSchema);
+export default model;
