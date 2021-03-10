a40e704e3ec158ca540fe21f4dcab24519da55f3
shinn92kr <64060313+shinn92kr@users.noreply.github.com>
2021-03-10 22:22:29 +0900

    #3.7_part 2(now video is working)

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(function (req, res, next) {
 // pug 사용 설정
 app.set("view engine", "pug");
 // Middleware
+app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
